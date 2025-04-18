be9402244624d0242cc3ac9a2fc4d3980a0fabf8
jose-sgariglia <josesgariglia@gmail.com>
2025-04-18 17:30:54 +0200

    + Example service in `service.py` + Example exploit in `example.py` * `.gitignore` for python enviroment ^ `exploit.py` in `client/exploits/`

---
+++ tests/service.py
@@ -0,0 +1,20 @@
+# ../venv/bin/python3
+
+from flask import Flask
+import random
+import string
+
+app = Flask(__name__)
+
+def generate_random_flag():
+    allowed_chars = string.ascii_uppercase + string.digits
+    random_part = ''.join(random.choice(allowed_chars) for _ in range(31))
+    return random_part + "="
+
+@app.route('/get-flag/')
+def get_flag():
+    flag = generate_random_flag()
+    return flag
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=False)

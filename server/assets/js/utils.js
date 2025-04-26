d5e46394916468b64a0af1e430fd4def4dbdb101
akiidjk <akiidjk@proton.me>
2025-04-26 16:53:36 +0200

    feat: enhance Tailwind CSS configuration and styles
    - Updated Tailwind CSS build commands to include configuration file.
    - Added custom scrollbar styles in global CSS.
    - Refactored API calls in JavaScript to remove hardcoded host URL.
    - Introduced a new function to generate UUIDs in utils.js.
    - Improved dashboard layout with a new flag submission interface.
    - Removed footer partial and adjusted header for logo inclusion.
    - Simplified pagination component and adjusted styles for better responsiveness.
    - Updated output CSS with new utility classes and styles.
    - Enhanced output.min.js with a new sendFlag function for flag submissions.
    - Expanded Tailwind CSS content paths to include partials and layouts.

--- server/assets/js/utils.js
+++ server/assets/js/utils.js
@@ -77,3 +77,13 @@ export function buildConfigFromDOM(document, tagify) {
     },
   };
 }
+
+
+export function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    .replace(/[xy]/g, function (c) {
+      const r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+}
--- server/tailwindcss.config.js
+++ server/tailwindcss.config.js
@@ -2,6 +2,6 @@ const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./internal/ui/views/**/*.html"],
+  content: ["./internal/ui/views/**/*.html", "./internal/ui/views/partials/**/*.html", "./internal/ui/views/layouts/**/*.html"],
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };

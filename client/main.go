42473a83539bd611b16be4b6a6a1686eb952dff0
akiidjk <dragonfaiver@gmail.com>
2025-06-01 13:39:05 +0200

    Hide banner during command completion

--- client/main.go
+++ client/main.go
@@ -3,14 +3,27 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/ByteTheCookies/cookieclient/cmd"
 )
+
+func isCompletionCommand() bool {
+	for _, arg := range os.Args {
+		if strings.Contains(arg, "__complete") || strings.Contains(arg, "completion") {
+			return true
+		}
+	}
+	return false
+}
 
 //go:embed banner.txt
 var banner string
 
 func main() {
-	fmt.Println(banner)
+	if !isCompletionCommand() {
+		fmt.Println(banner)
+	}
 	cmd.Execute()
 }

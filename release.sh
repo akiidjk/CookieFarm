a9ef05fd16ac61e333f9e9ffb7328c611520ed66
akiidjk <dragonfaiver@gmail.com>
2025-06-13 22:37:50 +0200

    Create release.sh

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+version=$1
+
+if [ -z "$version" ]; then
+  echo "Usage: $0 <version> (e.g., 1.0.0)"
+  exit 1
+fi
+
+git flow release finish "$version" --nodevelopmerge --nomainmerge -Fp
+
+git checkout -b temp-clean-release main
+
+git rm -r tests/ scripts/ monitoring/ RELEASE_TEMPLATE.md NOTES.md BENCHMARKS.md
+git commit -m "Pulizia file non destinati alla produzione"
+
+git checkout main
+git merge temp-clean-release
+
+git push origin main
+git push origin --tags
+
+git branch -d temp-clean-release

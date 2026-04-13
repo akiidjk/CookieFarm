519f9ffffd495f6985d970ce13a62ece2a9c208f
akiidjk <akiidjk@proton.me>
2026-04-13 18:41:58 +0200

    Move TUI and banner handling to parser
    Start TUI from parser and centralize banner printing there.
    Rename CLI flag to "banner" (defaults to true) and pass its
    address into buildCmd. Add configCheck in main that fetches the
    remote shared config, compares it to local, and prints a colored
    warning if they differ.

--- cookiefarm/client/cmd/parser.go
+++ cookiefarm/client/cmd/parser.go
@@ -2,14 +2,38 @@ package cmd
 
 import (
 	"context"
+	"logger"
 	"os"
+
+	"client/tui"
 
 	"charm.land/lipgloss/v2"
 	"github.com/charmbracelet/fang"
 )
 
-func ParseArgs(version string, theme fang.ColorScheme, useBanner *bool) {
-	buildCmd(useBanner)
+var (
+	useBanner bool = true
+	useTUI    bool
+)
+
+func startTui() {
+	err := tui.StartTUI(logger.GetBanner("client"))
+	if err != nil {
+		logger.Log.Error().Err(err).Msg("Error starting TUI")
+		if !logger.IsCompletionCommand() {
+			logger.PrintBanner(useBanner, "client")
+		}
+	}
+}
+
+func ParseArgs(version string, theme fang.ColorScheme) {
+	buildCmd(&useBanner)
+
+	if useTUI {
+		startTui()
+	} else if !logger.IsCompletionCommand() {
+		logger.PrintBanner(useBanner, "client")
+	}
 
 	err := fang.Execute(
 		context.TODO(),

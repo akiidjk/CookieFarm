ba05aa365c69612c9908ec334c8a7ef97a7f9420
akiidjk <dragonfaiver@gmail.com>
2026-04-21 23:19:04 +0200

    Remove Version field and sharedconfig import

--- cookiefarm/client/cmd/root.go
+++ cookiefarm/client/cmd/root.go
@@ -3,16 +3,14 @@ package cmd
 import (
 	"logger"
 	"os"
-	"sharedconfig"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:     "ckc",
-	Short:   "CLI client for interacting with the CookieFarm exploit framework",
-	Long:    `CookieFarm is an automated exploitation framework developed by the ByteTheCookies team for the CyberChallenge competition. This CLI client connects to the CookieFarm server to deploy and manage exploits against target teams. To launch the terminal-based user interface (TUI), simply run the command "ckc" without any arguments.`, //nolint:revive
-	Version: sharedconfig.GetVersion(),
+	Use:   "ckc",
+	Short: "CLI client for interacting with the CookieFarm exploit framework",
+	Long:  `CookieFarm is an automated exploitation framework developed by the ByteTheCookies team for the CyberChallenge competition. This CLI client connects to the CookieFarm server to deploy and manage exploits against target teams. To launch the terminal-based user interface (TUI), simply run the command "ckc" without any arguments.`, //nolint:revive
 }
 
 func buildCmd(useBanner *bool, useTUI *bool) *cobra.Command {

4a101ecdbf83fd089e484daf83c4904294ecfea3
akiidjk <dragonfaiver@gmail.com>
2026-04-23 20:20:57 +0200

    Remove useless file

--- cookiefarm/server/tailwindcss.config.js
+++
@@ -1,7 +0,0 @@
-const colors = require("tailwindcss/colors");
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./internal/ui/views/**/*.html", "./internal/ui/views/partials/**/*.html", "./internal/ui/views/layouts/**/*.html"],
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};

5396f0275f8858ac8a8771588275bd9e5c8414cf
akiidjk <akiidjk@proton.me>
2026-04-24 09:19:30 +0200

    Move login and logout commands to root

--- cookiefarm/client/cmd/args.go
+++ cookiefarm/client/cmd/args.go
@@ -72,11 +72,10 @@ func buildConfigCmd() *cobra.Command {
 
 	configCmd.AddCommand(resetConfigCmd)
 	configCmd.AddCommand(editConfigCmd)
-	configCmd.AddCommand(loginConfigCmd)
-	rootCmd.AddCommand(loginConfigCmd)
-	configCmd.AddCommand(logoutConfigCmd)
-	rootCmd.AddCommand(logoutConfigCmd)
 	configCmd.AddCommand(showConfigCmd)
+
+	rootCmd.AddCommand(loginConfigCmd)
+	rootCmd.AddCommand(logoutConfigCmd)
 
 	return configCmd
 }

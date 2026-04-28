ef43801ca25389bc1d191845383eda4ff01519d9
akiidjk <dragonfaiver@gmail.com>
2026-04-28 19:56:46 +0200

    Use ticks in Flag TTL log message

--- cookiefarm/server/internal/core/config.go
+++ cookiefarm/server/internal/core/config.go
@@ -31,7 +31,7 @@ func (r *Runner) Run() {
 	go r.StartFlagProcessingLoop(ctx)
 
 	if r.config.GetFlagTTL() != 0 {
-		logger.Log.Warn().Msgf("Flag TTL is set to %d seconds, starting validation loop", r.config.GetFlagTTL())
+		logger.Log.Warn().Msgf("Flag TTL is set to %d ticks, starting validation loop", r.config.GetFlagTTL())
 		go r.ValidateFlagTTL(ctx, r.config.GetFlagTTL(), uint64(r.config.GetTickTime()))
 	}
 }
